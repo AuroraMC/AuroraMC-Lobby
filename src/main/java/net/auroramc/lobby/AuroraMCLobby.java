82286021e01b83da241b3e50c01d584c9ff569f3
Block2Block <accounts@block2block.me>
2021-09-10 20:59:43 +0100

    Initial Commit

---
+++ src/main/java/net/auroramc/lobby/AuroraMCLobby.java
@@ -0,0 +1,12 @@
+package net.auroramc.lobby;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AuroraMCLobby extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
