63345e585b0a70d93ba6e1921784fdacb6d609d5
BuildTools <unconfigured@null.spigotmc.org>
2021-10-13 12:38:23 +0100

    restarting servers is annoying me so i added it so it will restart itself now

--- src/main/java/net/auroramc/lobby/AuroraMCLobby.java
+++ src/main/java/net/auroramc/lobby/AuroraMCLobby.java
@@ -1,12 +1,14 @@
 package net.auroramc.lobby;
 
+import net.auroramc.lobby.listeners.ShutdownEventListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AuroraMCLobby extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        Bukkit.getPluginManager().registerEvents(new ShutdownEventListener(), this);
     }
 
 }
---
+++ src/main/java/net/auroramc/lobby/listeners/ShutdownEventListener.java
@@ -0,0 +1,21 @@
+package net.auroramc.lobby.listeners;
+
+import net.auroramc.core.api.AuroraMCAPI;
+import net.auroramc.core.api.backend.communication.CommunicationUtils;
+import net.auroramc.core.api.backend.communication.Protocol;
+import net.auroramc.core.api.backend.communication.ProtocolMessage;
+import net.auroramc.core.api.events.ServerCloseRequestEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class ShutdownEventListener implements Listener {
+
+
+    @EventHandler
+    public void onShutdown(ServerCloseRequestEvent e) {
+        CommunicationUtils.sendMessage(new ProtocolMessage(Protocol.CONFIRM_SHUTDOWN, "Mission Control", e.getType(), AuroraMCAPI.getServerInfo().getName(), AuroraMCAPI.getServerInfo().getNetwork().name()));
+        CommunicationUtils.shutdown();
+    }
+
+
+}
