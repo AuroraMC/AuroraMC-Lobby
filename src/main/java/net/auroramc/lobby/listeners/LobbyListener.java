15ecf93edb67b1f046a7b11401e6aa26cd59b531
Block2Block <accounts@block2block.me>
2022-04-21 23:46:29 +0300

    Add lobby border
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/lobby/listeners/LobbyListener.java
+++ src/main/java/net/auroramc/lobby/listeners/LobbyListener.java
@@ -29,12 +29,14 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 
 public class LobbyListener implements Listener {
@@ -259,5 +261,54 @@ public class LobbyListener implements Listener {
         }
 
 
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent e) {
+        int highX = 0, lowX = 0, highY = 0, lowY = 0, highZ = 0, lowZ = 0;
+        JSONObject a = LobbyAPI.getLobbyMap().getMapData().getJSONObject("border_a");
+        JSONObject b = LobbyAPI.getLobbyMap().getMapData().getJSONObject("border_b");
+        if (a.getInt("x") > b.getInt("x")) {
+            highX = a.getInt("x");
+            lowX = b.getInt("x");
+        } else {
+            highX = b.getInt("x");
+            lowX = a.getInt("x");
+        }
+
+        if (a.getInt("y") > b.getInt("y")) {
+            highY = a.getInt("y");
+            lowY = b.getInt("y");
+        } else {
+            highY = b.getInt("y");
+            lowY = a.getInt("y");
+        }
+
+        if (a.getInt("z") > b.getInt("z")) {
+            highZ = a.getInt("z");
+            lowZ = b.getInt("z");
+        } else {
+            highZ = b.getInt("z");
+            lowZ = a.getInt("z");
+        }
+
+        if (e.getTo().getX() < lowX || e.getTo().getX() > highX || e.getTo().getY() < lowY || e.getTo().getY() > highY || e.getTo().getZ() < lowZ || e.getTo().getZ() > highZ) {
+            //Call entity damage event so the games can handle them appropriately.
+            JSONArray spawnLocations = LobbyAPI.getLobbyMap().getMapData().getJSONObject("spawn").getJSONArray("PLAYERS");
+            if (spawnLocations == null || spawnLocations.length() == 0) {
+                LobbyAPI.getLobby().getLogger().info("An invalid waiting lobby was supplied, assuming 0, 64, 0 spawn position.");
+                e.getPlayer().teleport(new Location(Bukkit.getWorld("world"), 0, 64, 0));
+            } else {
+                int x, y, z;
+                x = spawnLocations.getJSONObject(0).getInt("x");
+                y = spawnLocations.getJSONObject(0).getInt("y");
+                z = spawnLocations.getJSONObject(0).getInt("z");
+                float yaw = spawnLocations.getJSONObject(0).getFloat("yaw");
+                e.getPlayer().teleport(new Location(Bukkit.getWorld("world"), x, y, z, yaw, 0));
+            }
+            if (LobbyAPI.getLobbyMap().getMapData().getInt("time") > 12000) {
+                e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 1000000, 0, true, false), false);
+            }
+        }
     }
 }
