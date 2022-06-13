a49b0bd8b52b43c43b374f099a3787bf07481702
Block2Block <accounts@block2block.me>
2022-06-13 12:10:07 +0100

    Implement menu
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/lobby/gui/ViewCrates.java
+++ src/main/java/net/auroramc/lobby/gui/ViewCrates.java
@@ -4,12 +4,24 @@
 
 package net.auroramc.lobby.gui;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import net.auroramc.core.api.AuroraMCAPI;
 import net.auroramc.core.api.players.AuroraMCPlayer;
 import net.auroramc.core.api.utils.gui.GUI;
+import net.auroramc.core.api.utils.gui.GUIItem;
 import net.auroramc.lobby.AuroraMCLobby;
 import net.auroramc.lobby.api.players.AuroraMCLobbyPlayer;
+import org.bukkit.Material;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.material.Skull;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.UUID;
 
 public class ViewCrates extends GUI {
 
@@ -19,6 +31,95 @@ public class ViewCrates extends GUI {
         super("&3&lCrate Menu", 2, true);
         border("&3&lCrate Menu", null);
         this.player = player;
+
+        GameProfile profile = new GameProfile(UUID.randomUUID(), null);
+        profile.getProperties().put("textures", new Property("textures", Base64.getEncoder().encodeToString("{textures:{SKIN:{url:\"http://textures.minecraft.net/texture/f7aadff9ddc546fdcec6ed5919cc39dfa8d0c07ff4bc613a19f2e6d7f2593\"}}}".getBytes())));
+        ItemStack head = new ItemStack(Material.SKULL_ITEM, 1);
+        head.setDurability((short)3);
+        SkullMeta meta = (SkullMeta) head.getItemMeta();
+        meta.setDisplayName(AuroraMCAPI.getFormatter().convert(AuroraMCAPI.getFormatter().convert("&7&lIron Crate")));
+        meta.setLore(Arrays.asList(AuroraMCAPI.getFormatter().convert(AuroraMCAPI.getFormatter().highlight(";&7Iron Crates are the most common;&7and have the lowest chances to win awesome;&7stuff.;;&rCost: &a2,000 Tickets;;&aLeft-click to view available crates!;&aRight-click to purchase!")).split(";")));
+        Field field;
+        try {
+            field = meta.getClass().getDeclaredField("profile");
+        } catch (NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
+            return;
+        }
+        field.setAccessible(true);
+        try {
+            field.set(meta, profile);
+        } catch (IllegalAccessException | IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        head.setItemMeta(meta);
+        this.setItem(1, 1, new GUIItem(head));
+
+        profile = new GameProfile(UUID.randomUUID(), null);
+        profile.getProperties().put("textures", new Property("textures", Base64.getEncoder().encodeToString("{textures:{SKIN:{url:\"http://textures.minecraft.net/texture/487ff5845bd61e98b16d21915f700edf454497acd3d5a7ae2dbefccacbd5abe3\"}}}".getBytes())));
+        head = new ItemStack(Material.SKULL_ITEM, 1);
+        head.setDurability((short)3);
+        meta = (SkullMeta) head.getItemMeta();
+        meta.setDisplayName(AuroraMCAPI.getFormatter().convert(AuroraMCAPI.getFormatter().convert("&6&lGold Crate")));
+        meta.setLore(Arrays.asList(AuroraMCAPI.getFormatter().convert(AuroraMCAPI.getFormatter().highlight(";&7Gold Crates are rare crates that contain;&7more legendary loot!.;;&rCost: &a10,000 Tickets;;&aLeft-click to view available crates!;&aRight-click to purchase!")).split(";")));
+        try {
+            field = meta.getClass().getDeclaredField("profile");
+        } catch (NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
+            return;
+        }
+        field.setAccessible(true);
+        try {
+            field.set(meta, profile);
+        } catch (IllegalAccessException | IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        head.setItemMeta(meta);
+        this.setItem(1, 3, new GUIItem(head));
+
+        profile = new GameProfile(UUID.randomUUID(), null);
+        profile.getProperties().put("textures", new Property("textures", Base64.getEncoder().encodeToString("{textures:{SKIN:{url:\"http://textures.minecraft.net/texture/25807cc4c3b6958aea6156e84518d91a49c5f32971e6eb269a23a25a27145\"}}}".getBytes())));
+        meta.setLore(Arrays.asList(AuroraMCAPI.getFormatter().convert(AuroraMCAPI.getFormatter().highlight(";&7Diamond Crates are legendary crates that;&7can not just give you cosmetics, but can also give you;&ra rank upgrade!!.;;&rCost: &a20,000 Tickets;;&aLeft-click to view available crates!;&aRight-click to purchase!")).split(";")));
+        head = new ItemStack(Material.SKULL_ITEM, 1);
+        head.setDurability((short)3);
+        meta = (SkullMeta) head.getItemMeta();
+        meta.setDisplayName(AuroraMCAPI.getFormatter().convert(AuroraMCAPI.getFormatter().convert("&b&lDiamond Crate")));
+        try {
+            field = meta.getClass().getDeclaredField("profile");
+        } catch (NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
+            return;
+        }
+        field.setAccessible(true);
+        try {
+            field.set(meta, profile);
+        } catch (IllegalAccessException | IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        head.setItemMeta(meta);
+        this.setItem(1, 5, new GUIItem(head));
+
+        profile = new GameProfile(UUID.randomUUID(), null);
+        profile.getProperties().put("textures", new Property("textures", Base64.getEncoder().encodeToString("{textures:{SKIN:{url:\"http://textures.minecraft.net/texture/f3d5e43de5d4177c4baf2f44161554473a3b0be5430998b5fcd826af943afe3\"}}}".getBytes())));
+        head = new ItemStack(Material.SKULL_ITEM, 1);
+        head.setDurability((short)3);
+        meta = (SkullMeta) head.getItemMeta();
+        meta.setDisplayName(AuroraMCAPI.getFormatter().convert(AuroraMCAPI.getFormatter().convert("&a&lEmerald Crate")));
+        meta.setLore(Arrays.asList(AuroraMCAPI.getFormatter().convert(AuroraMCAPI.getFormatter().highlight(";&7Emerald Crates are legendary crates that not only;&7give you a very high chance to find;&7legendary loot, but also contains no duplicates!;;&cNote: &rThese chests can only be obtained;&rby claiming a Plus Bonus from The Monke.;;&aLeft-click to view available crates!")).split(";")));
+        try {
+            field = meta.getClass().getDeclaredField("profile");
+        } catch (NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
+            return;
+        }
+        field.setAccessible(true);
+        try {
+            field.set(meta, profile);
+        } catch (IllegalAccessException | IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        head.setItemMeta(meta);
+        this.setItem(1, 7, new GUIItem(head));
     }
 
     @Override
