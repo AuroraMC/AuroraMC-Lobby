000a46df82646a3a68fab5bb16fe81e89521861e
Block2Block <accounts@block2block.me>
2023-01-03 20:31:44 +0000

    Fix achievement given too early
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/lobby/api/parkour/ParkourRun.java
+++ src/main/java/net/auroramc/lobby/api/parkour/ParkourRun.java
@@ -247,7 +247,7 @@ public class ParkourRun {
                             break;
                         }
                         case 3: {
-                            if (finishMili <= 240000) {
+                            if (finishMili <= 330000) {
                                 if (!player.getStats().getAchievementsGained().containsKey(AuroraMCAPI.getAchievement(206))) {
                                     player.getStats().achievementGained(AuroraMCAPI.getAchievement(206), 1, true);
                                 }
