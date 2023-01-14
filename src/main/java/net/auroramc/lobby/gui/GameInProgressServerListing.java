74b61369e44d799e668e7fef58adb7bd3a0d6fb7
Block2Block <accounts@block2block.me>
2023-01-14 00:42:07 +0000

    Add ability to view inprogress servers
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/lobby/gui/GameInProgressServerListing.java
+++ src/main/java/net/auroramc/lobby/gui/GameInProgressServerListing.java
@@ -18,18 +18,17 @@ import org.bukkit.Sound;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class GameServerListing extends GUI {
+public class GameInProgressServerListing extends GUI {
 
     private final AuroraMCPlayer player;
     private final String gameCode;
     private final String gameName;
     private final String serverCode;
 
-    public GameServerListing(AuroraMCPlayer player, String gameCode, String gameName, String serverCode) {
+    public GameInProgressServerListing(AuroraMCPlayer player, String gameCode, String gameName, String serverCode) {
         super("&3&lSelect a server!", 5, true);
         this.player = player;
         this.gameCode = gameCode;
@@ -37,13 +36,13 @@ public class GameServerListing extends GUI {
         this.serverCode = serverCode;
         this.border("&3&lSelect a server!", null);
 
-        this.setItem(5, 4, new GUIItem(Material.WOOL, "&7&lSpectate In-Progress Games", 1, ";&r&aClick here to view games in progress!"));
+        this.setItem(0, 0, new GUIItem(Material.ARROW, "&c&lBack"));
 
-        List<GameServerInfo> infos = LobbyAPI.getGameServers().values().stream().filter(gameServerInfo -> gameServerInfo.getInfo().getServerType().getString("game").equalsIgnoreCase(gameCode) && (gameServerInfo.getServerState() == ServerState.STARTING || gameServerInfo.getServerState() == ServerState.WAITING_FOR_PLAYERS || gameServerInfo.getServerState() == ServerState.ACTIVE)).sorted((game1, game2) -> Integer.compare(game2.getCurrentPlayers(), game1.getCurrentPlayers())).collect(Collectors.toList());
+        List<GameServerInfo> infos = LobbyAPI.getGameServers().values().stream().filter(gameServerInfo -> gameServerInfo.getInfo().getServerType().getString("game").equalsIgnoreCase(gameCode) && (gameServerInfo.getServerState() == ServerState.IN_GAME || gameServerInfo.getServerState() == ServerState.ENDING)).sorted((game1, game2) -> Integer.compare(game2.getCurrentPlayers(), game1.getCurrentPlayers())).collect(Collectors.toList());
         int row = 2;
         int column = 2;
         for (GameServerInfo info : infos) {
-            this.setItem(row, column, new GUIItem(Material.STAINED_GLASS, "&3&l" + gameName + " Server " + info.getInfo().getName().split("-")[1], info.getCurrentPlayers(), ";&r&fPlayers: **" + info.getCurrentPlayers() + "**&f/**" + info.getMaxPlayers() + "**;&r&fGame: **" + info.getActiveGame() + "**" + ((!info.getActiveMap().equalsIgnoreCase("n/a"))?";&r&fMap: **" + info.getActiveMap() + "**":"") + ";&r&fStatus: **" + info.getServerState().getName() + "**;;" + ((info.getCurrentPlayers() == info.getMaxPlayers())?"&cThis server is currently full!":"&aClick to join the server!"), (short)((info.getCurrentPlayers() == info.getMaxPlayers() || info.getServerState() == ServerState.INACTIVE)?14:((info.getServerState() == ServerState.STARTING)?4:0))));
+            this.setItem(row, column, new GUIItem(Material.STAINED_GLASS, "&3&l" + gameName + " Server " + info.getInfo().getName().split("-")[1], info.getCurrentPlayers(), ";&r&fPlayers: **" + info.getCurrentPlayers() + "**&f/**" + info.getMaxPlayers() + "**;&r&fGame: **" + info.getActiveGame() + "**" + ((!info.getActiveMap().equalsIgnoreCase("n/a"))?";&r&fMap: **" + info.getActiveMap() + "**":"") + ";&r&fStatus: **" + info.getServerState().getName() + "**;;" + ((info.getCurrentPlayers() == info.getMaxPlayers())?"&cThis server is currently full!":"&aClick to join the server!"), (short)((info.getServerState() == ServerState.IN_GAME)?5:4)));
             column++;
             if (column == 7) {
                 row++;
@@ -58,8 +57,8 @@ public class GameServerListing extends GUI {
     @Override
     public void onClick(int row, int column, ItemStack item, ClickType clickType) {
         if (item.getType() != Material.STAINED_GLASS) {
-            if (item.getType() == Material.WOOL) {
-                GameInProgressServerListing sl = new GameInProgressServerListing(player, gameCode, gameName, serverCode);
+            if (item.getType() == Material.ARROW) {
+                GameServerListing sl = new GameServerListing(player, gameCode, gameName, serverCode);
                 sl.open(player);
                 AuroraMCAPI.openGUI(player, sl);
                 return;
